3690fb980fbdfded9c69445a7c7e6c2f8edb77b0
Vatsal <ramivatsal07@gmail.com>
2020-01-03 14:51:06 +0530

    added edit page and synced data across pages

--- notes-app.js
+++ notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -7,13 +7,15 @@ const filters = {
 renderNotes(notes, filters)
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
+    const id = uuidv4()
+
     notes.push({
-        id: uuidv4(),
+        id: id,
         title: '',
         body: ''
     })
     saveNotes(notes)
-    renderNotes(notes, filters)
+    location.assign(`/edit.html#${id}`)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
@@ -23,4 +25,11 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
+})
+
+window.addEventListener('storage', function (e) {
+    if (e.key === 'notes') {
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
 })
--- notes-functions.js
+++ notes-functions.js
@@ -28,10 +28,9 @@ const removeNote = function (id) {
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
-    const textEl = document.createElement('span')
+    const textEl = document.createElement('a')
     const button = document.createElement('button')
 
-<<<<<<< HEAD
     // Setup the remove note button
     button.textContent = 'x'
     noteEl.appendChild(button)
@@ -40,24 +39,14 @@ const generateNoteDOM = function (note) {
         saveNotes(notes)
         renderNotes(notes, filters)
     })
-=======
-    button.textContent = 'Remove'
-    noteEl.appendChild(button)
-    
->>>>>>> 1363578a90a6bf5b2bf00fcb4883b3f7ba040f3e
 
     // Setup the note title text
     if (note.title.length > 0) {
         textEl.textContent = note.title
     } else {
-<<<<<<< HEAD
         textEl.textContent = 'Unnamed note'
-=======
-        textEl.textContent = 'Unnamed Recipe'
->>>>>>> 1363578a90a6bf5b2bf00fcb4883b3f7ba040f3e
     }
-    noteEl.appendChild(textEl)
-
+    textEl.setAttribute('href', `/edit.html#${note.id}`)
     noteEl.appendChild(textEl)
 
     return noteEl
